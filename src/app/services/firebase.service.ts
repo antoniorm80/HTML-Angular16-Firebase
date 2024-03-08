5a9d0efec830d8a489e2bf37ef02948284c3121b
Antonio Rivera <antoniorm80@hotmail.com>
2024-03-07 21:53:06 -0600

    Implementando Realtime Database Firebase

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -7,16 +7,31 @@ import { HttpClient } from "@angular/common/http";
 export class FirebaseService {
 
   info: any = {};
+  equipo: any = [];
   cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
 
   constructor(public http:HttpClient) { 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
 
+  public carga_info() {
     this.http.get("assets/data/info.pages.json")
-      .subscribe( data => {
-        // console.log(data);
-        this.info = data;
-        this.cargada = true;
-        console.log(this.info);
-      })
+    .subscribe( data => {
+      // console.log(data);
+      this.info = data;
+      this.cargada = true;      
+    });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://portafolio-3227a-default-rtdb.firebaseio.com/Equipo.json")
+    .subscribe( data => {
+       console.log(data);
+      // this.equipo = JSON.stringify(data);
+      this.equipo = data;
+      this.cargada_sobre_nosotros = true;      
+    });
   }
 }
